c526eb4a3c2142fb2691343d49e907b8527d2fc1
Kamruzzaman-98 <kamruzzamank98@gmail.com>
2026-08-04 15:17:50 +0600

    add preloader and scroll process

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,3 +179,40 @@ item.style.display="none";
 
 
 });
+
+
+
+/*=================================
+PRELOADER
+==================================*/
+
+window.addEventListener("load",()=>{
+
+const preloader=document.getElementById("preloader");
+
+preloader.style.opacity="0";
+
+setTimeout(()=>{
+
+preloader.style.display="none";
+
+},500);
+
+});
+
+
+/*=================================
+SCROLL PROGRESS
+==================================*/
+
+window.addEventListener("scroll",()=>{
+
+const scrollTop=document.documentElement.scrollTop;
+
+const scrollHeight=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+const percent=(scrollTop/scrollHeight)*100;
+
+document.getElementById("progress-bar").style.width=percent+"%";
+
+});
